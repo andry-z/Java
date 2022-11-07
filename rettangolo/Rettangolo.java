19071d4356ac63a07f97cd5d22926709398adbf7
Andrea Zotti <70062506+andry-z@users.noreply.github.com>
2022-11-07 13:28:17 +0100

    Modifiche alla classe rettangolo e terminato TestRettangolo

--- rettangolo/Rettangolo.java
+++ rettangolo/Rettangolo.java
@@ -2,17 +2,20 @@ import java.util.scanner;
 
 public class Rettangolo{
     Scanner in = new Scanner(System.in);
-    public float base = 0.0F;
-    public float altezza = 0.0F;
-    public float perimetro = 0.0F;
-    public float area = 0.0F;
+    
+    public Rettangolo(){
+        public float base = 0.0F;
+        public float altezza = 0.0F;
+        public float perimetro = 0.0F;
+        public float area = 0.0F;
+    }
 
     System.out.println("Inserire un valore per la base: ");
     base = in.nextFloat();
     System.out.println("Inserire un valore per l'altezza: ");
     altezza = in.nextFloat();
 
-    public void ridimensiona(newBase, newAltezza){
+    public void ridimensiona(float newBase, float newAltezza){
         base = newBase;
         altezza = newAltezza;
     }
--- rettangolo/TestRettangolo.java
+++ rettangolo/TestRettangolo.java
@@ -2,11 +2,20 @@ public class Main
 { 
 	public static void main(String [] args)
 	{
+		System.out.println("Inserire i valori richiesti per il primo rettangolo: ");
 		Rettangolo rett1 = new Rettangolo();
+		System.out.println("Inserire i valori richiesti per il secondo rettangolo: ");
 		Rettangolo rett2 = new Rettangolo();
+		System.out.println("Inserire i valori richiesti per il terzo rettangolo: ");
 		Rettangolo rett3 = new Rettangolo();
+		float sommaArea = 0.0F;
+		float sommaPerimetro = 0.0F;
+		sommaArea = rett1.area() + rett2.area() + rett3.area();
+		sommaPerimetro = rett1.perimetro() + rett2.perimetro() + rett3.perimetro();
+		System.out.println("La somma dei perimetri è " + sommaPerimetro + ". La somma delle aree è " + sommaArea);
+		rett1.ridimensiona(20, 50);
+		sommaArea = rett1.area() + rett2.area() + rett3.area();
+		sommaPerimetro = rett1.perimetro() + rett2.perimetro() + rett3.perimetro();
+		System.out.println("La somma dei perimetri è " + sommaPerimetro + ". La somma delle aree è " + sommaArea);
 	}
-
-    
-
 }
