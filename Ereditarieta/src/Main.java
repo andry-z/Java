6d235f49cb6d95d7d7b0f55d6b638a9744d729e5
Andrea Zotti <70062506+andry-z@users.noreply.github.com>
2023-03-12 18:37:01 +0100

    Completato esercizio ereditarietà

--- Ereditarieta/src/Main.java
+++ Ereditarieta/src/Main.java
@@ -1,17 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        Dipendente dip1 = new Dipendente("Pippo", "Baudo", "1/1/1900", 5000);
-        Dipendente dip2 = new Dipendente("Biffi", "coglione", "1/2/1900", 7000);
-        Dipendente dip3 = new Dipendente("Caca", "pupu", "1/3/1900", 1000);
+        Dipendente dip1 = new Dipendente("Pippo", "Baudo", "1/1/1900", 5100);
+        Dipendente dip2 = new Dipendente("Mario", "Rossi", "2/2/1950", 7800);
+        Dipendente dip3 = new Dipendente("Paola", "Bianchi", "3/1/2000", 1250);
         if(dip1.guadagnaPiuDi(dip2)){
-            if(dip1.guadagnaPiuDi(dip3)){
-                if(dip2.guadagnaPiuDi(dip3)){
-                    System.out.println(dip3.stampa() + "\n" + dip2.stampa() + "\n" + dip1.stampa());
-                }else{
-                    System.out.println(dip2.stampa() + "\n" + dip3.stampa() + "\n" + dip1.stampa());
-                }
+            if(dip2.guadagnaPiuDi(dip3)){
+                System.out.println(dip3.stampa());
+                System.out.println(dip1.stampa());
+                System.out.println(dip2.stampa());
             }else{
-                System.out.println(dip2.stampa() + "\n" + dip1.stampa() + "\n" + dip3.stampa());
+                if(dip1.guadagnaPiuDi(dip3)){
+                    System.out.println(dip3.stampa());
+                    System.out.println(dip2.stampa());
+                    System.out.println(dip1.stampa());
+                }else{
+                    System.out.println(dip2.stampa());
+                    System.out.println(dip3.stampa());
+                    System.out.println(dip1.stampa());
+                }
+            }
+        }else{
+            if(dip1.guadagnaPiuDi(dip3)){
+                System.out.println(dip3.stampa());
+                System.out.println(dip1.stampa());
+                System.out.println(dip2.stampa());
+            }else{
+                if(dip2.guadagnaPiuDi(dip3)){
+                    System.out.println(dip1.stampa());
+                    System.out.println(dip3.stampa());
+                    System.out.println(dip2.stampa());
+                }else{
+                    System.out.println(dip1.stampa());
+                    System.out.println(dip2.stampa());
+                    System.out.println(dip3.stampa());
+                }
             }
         }
     }
